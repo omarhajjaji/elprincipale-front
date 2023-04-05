e4746b0ed35145626e2a8d61d1cc6a6141d429c4
Omar Hajjaji <omarhajjaji14@gmail.com>
2023-04-05 21:39:28 +0100

    Login etudiant

---
+++ src/app/login-etudiant/login-etudiant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-etudiant',
+  templateUrl: './login-etudiant.component.html',
+  styleUrls: ['./login-etudiant.component.css']
+})
+export class LoginEtudiantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
