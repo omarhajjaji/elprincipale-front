0e6b378cf748ef16a21be2322d45f67120d990c8
Medini Nada <nada.medini@etudiant-fsegt.utm.tn>
2023-04-23 17:22:22 +0100

    add-etudiant + login-admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginEtudiantComponent } from './login-etudiant/login-etudiant.component';
+import { AddEtudiantComponent } from './add-etudiant/add-etudiant.component';
+import { LoginAdminComponent } from './login-admin/login-admin.component';
+import { HomeAdminComponent } from './home-admin/home-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginEtudiantComponent
+    LoginEtudiantComponent,
+    AddEtudiantComponent,
+    LoginAdminComponent,
+    HomeAdminComponent
   ],
   imports: [
     BrowserModule
